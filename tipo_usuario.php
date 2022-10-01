209f75cd3204ca4764af958fa520ffb2b183db2f
Lucas Teixeira Ronchi <lucas0headshot@gmail.com>
2022-10-01 17:06:41 -0300

    Teste conexão DB com Tipo_Usuario
    Teste de conexão com o Banco de Dados, inserindo o campo Descrição da tabela Tipo_Usuario

--- tipo_usuario.php
+++ tipo_usuario.php
@@ -1,8 +1,28 @@
 <?php
     $descricao = isset($_GET['Descricao']) ? $_GET['Descricao'] : ''; // Existe?(Variável ?Verdaderiro :Falso)
-    echo '->' . $descricao;
 
     if ($descricao == ''){ //Teste descrição
         echo 'A descrição é obrigatória!';
         exit;
+    }
+
+    require_once 'conexao.php'; //Usar conexao.php
+
+    if ($descricao == 'Desenvolvedor'){ //Verificar nível do Usuário
+        $sql = "Insert into tipo_usuario (Descricao,  Nivel)
+	            Values ('$descricao', 3);"; //Insert da Descrição
+    }
+    if ($descricao == 'Administrador'){
+        $sql = "Insert into tipo_usuario (Descricao,  Nivel)
+	            Values ('$descricao', 2);";
+    }
+    if ($descricao == 'Usuário'){
+        $sql = "Insert into tipo_usuario (Descricao,  Nivel)
+	            Values ('$descricao', 1);";
+    }
+
+    if  ($conexao -> exec($sql)){ //Teste | Executar comando SQL
+        echo 'Descrição salva com sucesso!';
+    }else{
+        echo 'Erro ao salvar!';
     }
